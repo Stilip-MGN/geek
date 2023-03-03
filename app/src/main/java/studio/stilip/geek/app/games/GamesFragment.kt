f2d8f18fae492998c99e89d541672190f56e0df4
Andrew Krysh <vip.krysh@mail.ru>
2023-03-04 00:27:24 +0500

    сделал адаптер для игр

--- app/src/main/java/studio/stilip/geek/app/games/GamesFragment.kt
+++ app/src/main/java/studio/stilip/geek/app/games/GamesFragment.kt
@@ -3,16 +3,37 @@ package studio.stilip.geek.app.games
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import dagger.hilt.android.AndroidEntryPoint
 import studio.stilip.geek.R
 import studio.stilip.geek.databinding.FragmentGamesBinding
+import studio.stilip.geek.domain.Game
 
 @AndroidEntryPoint
 class GamesFragment : Fragment(R.layout.fragment_games) {
+
+    private val viewModel: GamesViewModel by viewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         val binding = FragmentGamesBinding.bind(view)
+
+        val adapter = GameAdapter { }
+        adapter.submitList(arr)
+
+        with(binding) {
+            recGames.adapter = adapter
+        }
+
     }
+
+    val arr = arrayListOf(
+        Game(name = "A", time = "30 min", countPlayers = "2-3", age = "10+"),
+        Game(
+            name = "B", time = "120 min", countPlayers = "1-3", age = "15+",
+            description = "Вышел в поле дед",
+            logo = "https://tesera.ru/images/items/1401961,3/200x200xpa/photo1.png"
+        ),
+    )
 }
---
+++ app/src/main/java/studio/stilip/geek/domain/Game.kt
@@ -0,0 +1,11 @@
+package studio.stilip.geek.domain
+
+data class Game(
+    val id: String = "",
+    val name: String = "",
+    val logo: String = "",
+    val time: String = "",
+    val countPlayers: String = "",
+    val age: String = "",
+    val description: String = "",
+)
