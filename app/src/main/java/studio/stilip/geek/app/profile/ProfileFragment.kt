a56e4532a65f0822f5856cecc7a391597b3442da
Andrew Krysh <vip.krysh@mail.ru>
2023-03-09 18:49:49 +0500

    добавил в тул бар меню

--- app/src/main/java/studio/stilip/geek/app/profile/ProfileFragment.kt
+++ app/src/main/java/studio/stilip/geek/app/profile/ProfileFragment.kt
@@ -1,10 +1,17 @@
 package studio.stilip.geek.app.profile
 
+import android.content.Context
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.flowWithLifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
@@ -21,6 +28,26 @@ import studio.stilip.geek.databinding.FragmentProfileBinding
 class ProfileFragment : Fragment(R.layout.fragment_profile) {
     private val hostViewModel: HostViewModel by activityViewModels()
     private val viewModel: ProfileViewModel by viewModels()
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        context as AppCompatActivity
+
+        context.addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.edit_menu, menu)
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                return when (menuItem.itemId) {
+                    R.id.menu_edit -> {
+                        true
+                    }
+                    else -> false
+                }
+            }
+        }, this, Lifecycle.State.RESUMED)
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
