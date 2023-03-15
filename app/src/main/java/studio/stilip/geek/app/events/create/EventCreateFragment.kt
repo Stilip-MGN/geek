9949a1384bd76b8e6ce770b6dcf0650e7d618164
Andrew Krysh <vip.krysh@mail.ru>
2023-03-15 16:15:23 +0500

    валидация в create

--- app/src/main/java/studio/stilip/geek/app/events/create/EventCreateFragment.kt
+++ app/src/main/java/studio/stilip/geek/app/events/create/EventCreateFragment.kt
@@ -3,8 +3,10 @@ package studio.stilip.geek.app.events.create
 import android.content.Context
 import android.os.Bundle
 import android.view.*
+import android.widget.AdapterView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.MenuProvider
+import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
@@ -19,7 +21,6 @@ import studio.stilip.geek.R
 import studio.stilip.geek.app.HostViewModel
 import studio.stilip.geek.app.events.edit.GameSpinnerAdapter
 import studio.stilip.geek.databinding.FragmentEventCreateBinding
-import studio.stilip.geek.domain.entities.Event
 import studio.stilip.geek.domain.entities.Game
 
 @AndroidEntryPoint
@@ -42,10 +43,7 @@ class EventCreateFragment : Fragment(R.layout.fragment_event_create) {
             override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
                 return when (menuItem.itemId) {
                     R.id.menu_done -> {
-                        viewModel.create(collectNewEvent())
-                        findNavController().navigate(
-                            R.id.action_navigation_event_create_to_events
-                        )
+                        viewModel.onCompleteClicked()
                         true
                     }
                     else -> false
@@ -67,6 +65,63 @@ class EventCreateFragment : Fragment(R.layout.fragment_event_create) {
 
         with(binding) {
             spinner.adapter = adapterGames
+
+            editEventName.doOnTextChanged { text, _, _, _ ->
+                viewModel.onEventNameChanged(text.toString())
+            }
+
+            editDate.doOnTextChanged { text, _, _, _ ->
+                viewModel.onDateChanged(text.toString())
+            }
+
+            editDescription.doOnTextChanged { text, _, _, _ ->
+                viewModel.onDescriptionChanged(text.toString())
+            }
+
+            editPlace.doOnTextChanged { text, _, _, _ ->
+                viewModel.onPlaceChanged(text.toString())
+            }
+
+            editMembersCount.doOnTextChanged { text, _, _, _ ->
+                viewModel.onMaxMembersChanged(text.toString())
+            }
+
+            spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                override fun onNothingSelected(parent: AdapterView<*>?) {}
+
+                override fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    position: Int,
+                    id: Long
+                ) {
+                    viewModel.onGameChanged(spinner.selectedItem as Game)
+                }
+            }
+
+            viewLifecycleOwner.lifecycleScope.launch {
+                viewModel.eventNameHelper
+                    .flowWithLifecycle(viewLifecycleOwner.lifecycle)
+                    .collect { res ->
+                        editEventNameLayout.helperText = getText(res)
+                    }
+            }
+        }
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewModel.eventCreated
+                .flowWithLifecycle(viewLifecycleOwner.lifecycle)
+                .collect { isSuccess ->
+                    when (isSuccess) {
+                        true -> {
+                            findNavController().navigate(
+                                R.id.action_navigation_event_create_to_events
+                            )
+                        }
+                        false -> {}
+                        null -> {}
+                    }
+                }
         }
 
         viewLifecycleOwner.lifecycleScope.launch {
@@ -78,28 +133,6 @@ class EventCreateFragment : Fragment(R.layout.fragment_event_create) {
                     }
                 }
 
-        }
-    }
-
-    private fun collectNewEvent(): Event {
-        with(binding) {
-            val eventName = editEventName.text.toString()
-            val gameId = (spinner.selectedItem as Game).id
-            val gameName = (spinner.selectedItem as Game).name
-            val place = editPlace.text.toString()
-            val description = editDescription.text.toString()
-            val date = editDate.text.toString()
-            val maxMembers = editMembersCount.text.toString().toInt()
-
-            return Event(
-                eventName = eventName,
-                gameId = gameId,
-                gameName = gameName,
-                place = place,
-                date = date,
-                description = description,
-                maxMembers = maxMembers
-            )
         }
     }
 
--- app/src/main/java/studio/stilip/geek/app/events/create/EventCreateViewModel.kt
+++ app/src/main/java/studio/stilip/geek/app/events/create/EventCreateViewModel.kt
@@ -5,7 +5,9 @@ import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
+import studio.stilip.geek.R
 import studio.stilip.geek.domain.entities.Event
+import studio.stilip.geek.domain.entities.Game
 import studio.stilip.geek.domain.usecase.event.CreateEventUseCase
 import studio.stilip.geek.domain.usecase.game.GetAllGamesUserUseCase
 import javax.inject.Inject
@@ -15,13 +17,82 @@ class EventCreateViewModel @Inject constructor(
     private val getAllGames: GetAllGamesUserUseCase,
     private val createEvent: CreateEventUseCase
 ) : ViewModel() {
+
+    private val _eventName = MutableStateFlow("")
+    private val _game = MutableStateFlow(Game())
+    private val _place = MutableStateFlow("")
+    private val _date = MutableStateFlow("")
+    private val _description = MutableStateFlow("")
+    private val _maxMembers = MutableStateFlow("")
+
+    val eventCreated = MutableStateFlow<Boolean?>(null)
+    val eventNameHelper = MutableStateFlow(R.string.error_input_empty)
     val games = getAllGames()
         .stateIn(viewModelScope, SharingStarted.Eagerly, emptyList())
 
-
-    fun create(event: Event) {
+    fun onEventNameChanged(name: String) {
+        if (name == _eventName.value) return
         viewModelScope.launch {
-            createEvent(event)
+            if (name.isEmpty())
+                eventNameHelper.value = R.string.error_input_empty
+            else
+                eventNameHelper.value = R.string.empty_text
+            _eventName.value = name
+        }
+    }
+
+    fun onGameChanged(game: Game) {
+        if (game == _game.value) return
+        viewModelScope.launch {
+            _game.value = game
+        }
+    }
+
+    fun onPlaceChanged(place: String) {
+        if (place == _place.value) return
+        viewModelScope.launch {
+            _place.value = place
+        }
+    }
+
+    fun onDateChanged(date: String) {
+        if (date == _date.value) return
+        viewModelScope.launch {
+            _date.value = date
+        }
+    }
+
+    fun onDescriptionChanged(description: String) {
+        if (description == _description.value) return
+        viewModelScope.launch {
+            _description.value = description
+        }
+    }
+
+    fun onMaxMembersChanged(maxMembers: String) {
+        if (maxMembers == _maxMembers.value) return
+        viewModelScope.launch {
+            _maxMembers.value = maxMembers
+        }
+    }
+
+    fun onCompleteClicked() {
+        if (_eventName.value.isEmpty()) return
+        viewModelScope.launch {
+            val maxMembers = _maxMembers.value
+
+            createEvent(
+                Event(
+                    eventName = _eventName.value,
+                    gameId = _game.value.id,
+                    gameName = _game.value.name,
+                    place = _place.value,
+                    date = _date.value,
+                    description = _description.value,
+                    maxMembers = if (maxMembers.isEmpty()) 1 else maxMembers.toInt()
+                )
+            )
+            eventCreated.value = true
         }
     }
 }
